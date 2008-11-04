47d0d672c9565b03b4d90061878702ec9a660bf8
voldern <voldern@ebf9eed1-504e-0410-a380-a51402bba26a>
2008-11-04 19:37:39 +0000

    Made it possible to set default value for allow-transfer and added error reporting
    git-svn-id: https://wmbind.svn.sourceforge.net/svnroot/wmbind/trunk@56 ebf9eed1-504e-0410-a380-a51402bba26a

--- web/c/options.php
+++ web/c/options.php
@@ -13,6 +13,20 @@ class Controller_Options extends Controller_Application
 
 	function index()
 	{
+
+		// Save any changes
+		if ($this->request == "POST" && $this->Option->validate()) {
+	
+			// Reset all options
+			$this->Option->query("UPDATE `options` SET `prefval` = 'off' WHERE `preftype` = 'record'");
+
+			foreach ($_POST as $key => $value) {
+				if(!$this->Option->query("UPDATE `options` SET `prefval` = '{$value}' WHERE `prefkey` = '{$key}'"))
+					$this->template->error = "Could not update all options <br />\n";
+			}
+		}
+
+		// Get data to fill in the view
 		$records = $this->Option->findAll("`preftype` = 'record'", NULL, array('prefkey', 'prefval'), '`prefkey`');
 		for ($x = 0, $y = 0, $i = 0; $i < count($records); $y++, $i++) {
 			if ($y == 5) {
@@ -30,21 +44,6 @@ class Controller_Options extends Controller_Application
 		$this->template->records = $recordsArray; 
 	}
 
-	function edit()
-	{
-		if ($this->request == "POST") {
-			// Reset all options
-			$this->Option->query("UPDATE `options` SET `prefval` = 'off' WHERE `preftype` = 'record'");
-
-			foreach ($_POST as $key => $value) {
-				if (!$this->Option->query("UPDATE `options` SET `prefval` = '{$value}' WHERE `prefkey` = '{$key}'"))
-					$this->redirect('/options/');
-			}
-
-			$this->redirect('/options/');
-		}
-	}
 }
-
 
 ?>
