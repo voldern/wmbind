19b2f209204e8bdfcb43b27582f6ea800cb28133
Espen Volden <voldern@loge.webaro.net>
2009-04-07 14:01:03 +0200

    Updated the config.example.php so that it has examples of all options available

--- web/include/config.example.php
+++ web/include/config.example.php
@@ -4,12 +4,18 @@
 // WARNING: Changing this during production will make all current accounts invalid
 $this->passwordHash = '¡@3kDKAS9123:.213if;2,31ujasd1__123';
 
+// Should WMBIND be backwards compatible with SMBIND? (WARNING: This will lead to weaker security and the hash above will be ignored)
+$this->compability = false;
+
 // Database config
 $this->db_type = 'mysql'; // mysql for MySQL, pgsql for PostgreSQL
 $this->db_user = ''; // Database username
 $this->db_pass = ''; // Database password
 $this->db_host = 'localhost'; // Database host
 $this->db_db = ''; // Database name
+
+// Full path to Smarty.class.php
+$this->smarty_path = '/var/www/smarty/Smarty.class.php';
 
 // Zone data paths
 $this->zones_path = "/etc/bind/"; // Path to where to store zone files
@@ -19,5 +25,3 @@ $this->conf_path = "/etc/smbind/smbind.conf"; // Include this file in named.conf
 $this->namedcheckconf = '/usr/sbin/named-checkconf';
 $this->namedcheckzone = '/usr/sbin/named-checkzone';
 $this->rndc = '/usr/sbin/rndc';
-
-?>
